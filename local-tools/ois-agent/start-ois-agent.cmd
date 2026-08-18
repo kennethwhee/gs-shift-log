af07f7ec4a2d20590a4021d7c00ee1c2967c2ae6
kennethwhee <wheekeun.lee@gmail.com>
2026-08-19 08:55:20 +0900

    OIS Agent 시스템 CA 자동실행 고정

--- local-tools/ois-agent/start-ois-agent.cmd
+++ local-tools/ois-agent/start-ois-agent.cmd
@@ -7,6 +7,6 @@ set "NODE_USE_SYSTEM_CA=1"
 
 cd /d "%OIS_AGENT_DIR%"
 
-"%NODE_EXE%" "%OIS_AGENT_DIR%ois-login.js" >> "%OIS_AGENT_DIR%ois-agent.log" 2>&1
+"%NODE_EXE%" --use-system-ca "%OIS_AGENT_DIR%ois-login.js" >> "%OIS_AGENT_DIR%ois-agent.log" 2>&1
 
 endlocal
